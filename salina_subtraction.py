c7dfd914152f393eb13a6b75272e7bc32f322a0e
Salina2060 <166965942+Salina2060@users.noreply.github.com>
2025-03-24 11:15:37 +0545

    Added subtraction operation

---
+++ salina_subtraction.py
@@ -0,0 +1,11 @@
+# dev2_subtraction.py
+def subtract_numbers(a, b):
+    return a - b
+
+# Taking input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calling the function and displaying the result
+result = subtract_numbers(num1, num2)
+print(f"The result of subtraction is: {result}")
